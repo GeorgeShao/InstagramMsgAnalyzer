bac2ec12d7c702d3a19b5e7ca9649affd170894e
George Shao <GeorgeShao123@gmail.com>
2019-08-29 17:23:36 -0400

    Fixed epoch/unix timestamp for instagram custom heart msgs to be an integer.

--- InstagramMsgAnalyzer.py
+++ InstagramMsgAnalyzer.py
@@ -1,6 +1,7 @@
 import json # JSON file managing library
 import dateutil.parser as dp # ISO 8601 to Epoch time conversion library
 from textblob import TextBlob # sentiment analysis library
+import time
 
 # import pyperclip; print = pyperclip.copy
 
@@ -10,18 +11,25 @@ from textblob import TextBlob # sentiment analysis library
 
 ENABLE_SENTIMENT_ANALYSIS = False
 
-a = json.loads(open('messages.json').read())
+json_file = json.loads(open('messages.json').read())
 
-for i in a:
+text_msgs = 0
+heart_msgs = 0
+
+for i in json_file:
     for j in i['conversation']:
         if 'text' in j:
             if ENABLE_SENTIMENT_ANALYSIS:
-                text_to_be_analyzed = j['text']
-                blob = TextBlob(text_to_be_analyzed)
-                print(f"[{int(dp.parse(j['created_at']).timestamp())}] [{blob.sentiment.polarity}] {j['sender']}: {j['text']}")
+                analyzed_text = TextBlob(j['text'])
+                print(f"[{int(dp.parse(j['created_at']).timestamp())}] [{analyzed_text.sentiment.polarity}] {j['sender']}: {j['text']}")
             else:
                 print(f"[{int(dp.parse(j['created_at']).timestamp())}] [N/A] {j['sender']}: {j['text']}")
+            text_msgs += 1
         else:
-            print(f"[{dp.parse(j['created_at']).timestamp()}] {j['sender']}: (HEART)") # TODO: sentiment analysis on heart based on previous msg(s) sent
-        
-    print('-'*10+'CONVERSATION_BREAK'+'-'*10)
+            print(f"[{int(dp.parse(j['created_at']).timestamp())}] [N/A] {j['sender']}: (HEART)") #TODO: sentiment analysis on heart based on previous msg(s) sent
+            heart_msgs += 1
+    print('-'*10 + 'CONVERSATION_BREAK' + '-'*10)
+
+print("text_msgs: " + str(text_msgs))
+print("heart_msgs: " + str(heart_msgs))
+print("total_msgs: " + str(text_msgs + heart_msgs))
